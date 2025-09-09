da6563968a8d5ba579eb5b1c72ca6159ed633eb2
Gabriel Sanchez <gabriel.sanchez23@upr.edu>
2025-09-09 03:41:02 +0000

    Employee Class

--- HourlyEmployee.cpp
+++ HourlyEmployee.cpp
@@ -1,9 +1,28 @@
 #include "HourlyEmployee.h"
 
 HourlyEmployee::HourlyEmployee(const std::string& name, int id, 
-                                double payPerHour, double workedHours)
-{
+double payPerHour, double workedHours)
+    :Employee(name, id, "Hourly", payPerHour, workedHours, 0.0){
+    this->workedHours = workedHours;
+    }
+
     // TODO: 
+
+double HourlyEmployee::GetWorkedHours(){
+    return workedHours;
 }
-/*TODO: calculate Payment */
+
+double HourlyEmployee::getPayPerHour(){
+    return payPerHour;
+
+}
+/*TODO: calculate Payment */   // done
+
+double HourlyEmployee::calculatePay() const {
+    return (workedHours * payPerHour)/ 26;
+}
+
+
+
+
 
--- HourlyEmployee.h
+++ HourlyEmployee.h
@@ -7,9 +7,10 @@ class HourlyEmployee : public Employee {
 public:
     double payPerHour;
     double workedHours;
-
+    HourlyEmployee();
     HourlyEmployee(const std::string& name, int id, double payPerHour, double workedHours);
-
+    double getPayPerHour();
+    double GetWorkedHours();
     double calculatePay() const override;
 };
 
